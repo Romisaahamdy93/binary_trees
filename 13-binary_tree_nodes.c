ff81bbb8e2e99b495460ffc4a7be7503a3a3466c
Romisaa <romisaa.hamdy@yahoo.com>
2024-02-28 12:15:02 -0800

    task13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,7 +10,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
-size_t left_nodes = binary_tree_nodes(tree->left);
-size_t right_nodes = binary_tree_nodes(tree->right);
-return (tree->left != NULL || tree->right != NULL) + left_nodes + right_nodes;
+size_t nodes = 0;
+if (tree)
+{
+nodes += (tree->left || tree->right) ? 1 : 0;
+nodes += binary_tree_nodes(tree->left);
+nodes += binary_tree_nodes(tree->right);
+}
+return (nodes);
 }
