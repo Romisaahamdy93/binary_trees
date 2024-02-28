315ec79751dc65134fdc4bd9fd02847286712965
“Dino” <daringamaleldin@hotmail.com>
2024-02-28 10:40:46 -0800

    update

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,12 +9,12 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+if (tree == NULL || func == NULL)
+return;
 
-    func(tree->n);  // Call the provided function for the current node
+func(tree->n);
 
-    // Recursively traverse the left and right subtrees
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
 }
